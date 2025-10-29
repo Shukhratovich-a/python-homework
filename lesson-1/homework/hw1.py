0770a777bfc68b4a5997193a0b08f0c7f2cb366f
Shukhratovich-a <94617261+Shukhratovich-a@users.noreply.github.com>
2025-10-29 14:36:34 +0500

    finish 1st homework

---
+++ lesson-1/homework/hw1.py
@@ -0,0 +1,26 @@
+# 1
+a = float(input("Side of square: "))
+p = 4 * a
+s = a ** 2
+print("Perimeter:", p)
+print("Area:", s)
+
+# 2
+import math
+d = float(input("Diameter of circle: "))
+l = math.pi * d
+print("Length:", l)
+
+# 3
+a = float(input("a: "))
+b = float(input("b: "))
+mean = (a + b) / 2
+print("Mean:", mean)
+
+# 4
+a = float(input("a: "))
+b = float(input("b: "))
+print("Sum:", a + b)
+print("Product:", a * b)
+print("Square of a:", a ** 2)
+print("Square of b:", b ** 2)
